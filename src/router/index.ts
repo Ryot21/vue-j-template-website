d718d4e868cbfc9d111c2a608c50a2cc85345779
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-03-05 23:12:41 +0900

    Vue Router > ページタイトル機能追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,21 +5,64 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      title: '物件名 | 住協グループの住まい情報',
+      des: '物件の説明が入ります。'
+    },
+  },{
+    path: '/concept',
+    name: 'concept',
+    component: () => import('../views/ConceptView.vue'),
+    meta: {
+      title: 'コンセプト | 住協グループの住まい情報',
+      des: 'コンセプトページの説明が入ります。'
+    },
+  },{
+    path: '/layout',
+    name: 'layout',
+    component: () => import('../views/LayoutView.vue'),
+    meta: {
+      title: 'レイアウト | 住協グループの住まい情報',
+      des: 'レイアウトページの説明が入ります。'
+    }
+  },{
+    path: '/plan',
+    name: 'plan',
+    component: () => import('../views/PlanView.vue'),
+    meta: {
+      title: 'プラン | 住協の住まい情報',
+      meta: 'プランページの説明文が入ります。'
+    }
+  },{
+    path: '/euipment',
+    name: 'euipment',
+    component: () => import('../views/EuipmentView.vue'),
+    meta: {
+      title: '設備・仕様 | 住協グループの住まい情報',
+      meta: '設備・仕様ページの説明文が入ります。'
+    }
+  },{
+    path: '/location',
+    name: 'location',
+    component: () => import('../views/LocationView.vue'),
+    meta: {
+      title: '周辺環境 | 住協グループの住まい情報',
+      meta: '周辺環境ページの説明文が入ります。'
+    }
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (typeof to.meta.title === 'string') {
+    document.title = to.meta.title;
+  }
+  next();
+});
 
 export default router
