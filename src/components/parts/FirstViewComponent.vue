492eaae85bf7dac8175b0ea17b70e37712a78288
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-03-10 23:52:23 +0900

    Vue > Component > ディレクトリ構造変更
    ・Parts : サイトを構成する骨組みのパーツ（Header, Footer）
    ・introduction : オープンソースを利用したパーツ

---
+++ src/components/parts/FirstViewComponent.vue
@@ -0,0 +1,24 @@
+<script>
+	export default{name: 'TopFview'}
+</script>
+
+
+
+<template>
+<!-- ファーストビュー -->
+        <div class="c-contents -imgBg">
+            <div class="imgBox">
+                <SwiperComponent />
+            </div>
+            <div class="sp c-contentsBox -f-logo_s">
+                <div class="c-contents -imgBg">
+                    <!-- SPサイズ：w300px × h108px -->
+                    <div class="imgBox">
+                        <img src="https://via.placeholder.com/300x108/?text=SP 300x108">
+                    </div>
+                    <div class="c-contentsBox -logo-item01 a-moveUp"><img src="images/logo/site-logo-bird01.png" alt="動かす画像1"></div>
+                    <div class="c-contentsBox -logo-item02 a-moveDown"><img src="images/logo/site-logo-bird02.png" alt="動かす画像2"></div>
+                </div>
+            </div>
+        </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import HeaderComponent   from './components/HeaderComponent.vue'
-import FooterComponent   from './components/FooterComponent.vue'
-import MenuComponent     from './components/MenuComponent.vue'
-import TopFviewComponent from './components/TopFviewComponent.vue'
+import HeaderComponent    from './components/parts/HeaderComponent.vue'
+import FooterComponent    from './components/parts/FooterComponent.vue'
+import MenuComponent      from './components/parts/MenuComponent.vue'
+import FirstViewComponent from './components/parts/FirstViewComponent.vue'
+import SwiperComponent    from './components/introduction/SwiperComponent.vue'
 
 const app = createApp(App)
 
 app.use(router)
 app.component('HeaderComponent', HeaderComponent)
-app.component('MenuComponent', MenuComponent)
 app.component('FooterComponent', FooterComponent)
-app.component('TopFview', TopFviewComponent)
+app.component('MenuComponent', MenuComponent)
+app.component('FirstViewComponent', FirstViewComponent)
+app.component('SwiperComponent', SwiperComponent)
 app.mount('#app')
