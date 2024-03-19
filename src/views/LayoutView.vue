ee508f88f102fc2bd5e269f938a64ee1e64d8e26
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-03-19 21:02:41 +0900

    Vue化完了

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,108 +1,108 @@
 <template>
-  <div class="layout mgb10 mgb10s">
-    <FirstViewComponent02/>
-    <!-- コンテンツ -->
-    <div class="c-contents w1100">
-        <div class="c-contents-inner">
-            <!-- コンテンツ > タイトル -->
-            <h2 class="c-contents_MainTitle -icon -left -lh-n mgb2 mgb3s">
-                ページタイトル
-            </h2>
-            <!-- コンテンツ > 説明 -->
-            <p class="s-M -f-gothic -left -lh-1_5 -lh-1_5_s mgb20 mgb20s a-fadeUp -sc40">
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br class=""><br>
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br><br>
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br><br>
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br><br>
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
-            </p>
-        </div>
-        <!-- ----------------【PC2カラム】 ------------------- -->
-        <div class="tb-pc c-contents w95 mgb10 mgb10s">
-            <h2 class="c-contents_title -layout s-LL -center mgb10 mgb10s">コンテンツタイトル</h2>
-            <article>
-                <ul class="c-flex -col1_2 -cr_s">
-                    <li class="text">
-                        <div class="mgb15 mgb10s">
-                            <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm -dot s-L -left a-fadeLeft -sc40">コンテンツアイテム_タイトル</p>
-                            <p class="s-M -lh-2 -left a-fadeLeft -sc40">
+    <div class="layout mgb10 mgb10s">
+        <FirstViewComponent02/>
+        <!-- コンテンツ -->
+        <div class="c-contents w1100">
+            <div class="c-contents-inner">
+                <!-- コンテンツ > タイトル -->
+                <h2 class="c-contents_MainTitle -icon -left -lh-n mgb2 mgb3s">
+                    ページタイトル
+                </h2>
+                <!-- コンテンツ > 説明 -->
+                <p class="s-M -f-gothic -left -lh-1_5 -lh-1_5_s mgb20 mgb20s a-fadeUp -sc40">
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br class=""><br>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br><br>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br><br>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。<br><br>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
+                </p>
+            </div>
+            <!-- ----------------【PC2カラム】 ------------------- -->
+            <div class="tb-pc c-contents w95 mgb10 mgb10s">
+                <h2 class="c-contents_title -layout s-LL -center mgb10 mgb10s">コンテンツタイトル</h2>
+                <article>
+                    <ul class="c-flex -col1_2 -cr_s">
+                        <li class="text">
+                            <div class="mgb15 mgb10s">
+                                <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm -dot s-L -left a-fadeLeft -sc40">コンテンツアイテム_タイトル</p>
+                                <p class="s-M -lh-2 -left a-fadeLeft -sc40">
+                                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
+                                </p>
+                            </div>
+                            <div class="mgb5 mgb10s">
+                                <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm -dot s-L -left a-fadeLeft -sc40">コンテンツアイテム_タイトル</p>
+                                <p class="s-M -lh-2 -left a-fadeLeft -sc40">
+                                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
+                                </p>
+                            </div>
+                        </li>
+                        <li class="img mgb8s">
+                            <img src="https://via.placeholder.com/630x473/?text=ALL 630x473(4:3)">
+                        </li>
+                    </ul>
+                </article>
+            </div>
+            <!-- ---------------- 【SP・PC1カラム】 ------------------- -->
+            <div class="c-contents w95 mgb10 mgb10s">
+                <h2 class="c-contents_title -sample s-LL -center mgb10 mgb10s">コンテンツタイトル</h2>
+                <img class="mgb5 mgb5s" src="https://via.placeholder.com/1100x619/?text=ALL 1100x619(16:9)">
+                <!-- ボーダーボックス -->
+                <div class="c-borderBox -double mgb10 mgb10s">
+                    <p class="s-L -center"><span class="s-3L -b">3</span>LDK + 特徴その1 + 特徴その2</p>
+                </div>
+                <div class="sp mgb20s">
+                    <ul class="c-flex -col2_2">
+                        <li>
+                            <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm s-L -left pdl7s">コンテンツアイテム_タイトル</p>
+                            <p class="s-M -left -lh-1_5_s">
                                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
                             </p>
-                        </div>
-                        <div class="mgb5 mgb10s">
-                            <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm -dot s-L -left a-fadeLeft -sc40">コンテンツアイテム_タイトル</p>
-                            <p class="s-M -lh-2 -left a-fadeLeft -sc40">
+                        </li>
+                        <li>
+                            <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm s-L -left pdl7s">コンテンツアイテム_タイトル</p>
+                            <p class="s-M -left -lh-1_5_s">
                                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
                             </p>
-                        </div>
-                    </li>
-                    <li class="img mgb8s">
-                        <img src="https://via.placeholder.com/630x473/?text=ALL 630x473(4:3)">
-                    </li>
-                </ul>
-            </article>
-        </div>
-        <!-- ---------------- 【SP・PC1カラム】 ------------------- -->
-        <div class="c-contents w95 mgb10 mgb10s">
-            <h2 class="c-contents_title -sample s-LL -center mgb10 mgb10s">コンテンツタイトル</h2>
-            <img class="mgb5 mgb5s" src="https://via.placeholder.com/1100x619/?text=ALL 1100x619(16:9)">
-            <!-- ボーダーボックス -->
-            <div class="c-borderBox -double mgb10 mgb10s">
-                <p class="s-L -center"><span class="s-3L -b">3</span>LDK + 特徴その1 + 特徴その2</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="sp mgb20s">
-                <ul class="c-flex -col2_2">
-                    <li>
-                        <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm s-L -left pdl7s">コンテンツアイテム_タイトル</p>
-                        <p class="s-M -left -lh-1_5_s">
-                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
-                        </p>
-                    </li>
-                    <li>
-                        <p class="c-contents_subTitle -iconBox -iconBtm -borderBtm s-L -left pdl7s">コンテンツアイテム_タイトル</p>
-                        <p class="s-M -left -lh-1_5_s">
-                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
-                        </p>
-                    </li>
-                </ul>
+            <!-- ---------------- 【SP2カラム、PC2カラム】 ------------------- -->
+            <div class="c-contents w95 mgb10 mgb10s">
+                <article>
+                    <ul class="c-flex -h-c_pc -col2_2 -r mgb10 mgb10s">
+                        <li class="">
+                            <p class="c-contents_subTitle s-L -left mgb5 mgb5s a-fadeRight -sc40">コンテンツアイテム_タイトル</p>
+                            <p class="s-M -lh-2 -lh-1_5_s -left a-fadeRight -sc40">
+                                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
+                            </p>
+                        </li>
+                        <li class=" a-flipLeft -sc40">
+                            <img src="https://via.placeholder.com/475x320/?text=480x320">
+                            <p class="-right a-fade -sp4"><span class="s-kome -color03">※イメージ</span></p>
+                        </li>
+                    </ul>
+                </article>
+
+                <article>
+                    <ul class="c-flex -h-c_pc -col2_2">
+                        <li class="">
+                            <p class="c-contents_subTitle s-L -left mgb5 mgb5s a-fadeLeft -sc40">コンテンツアイテム_タイトル</p>
+                            <p class="s-M -lh-2 -lh-1_5_s -left a-fadeLeft -sc40">
+                                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
+                            </p>
+                        </li>
+                        <li class="a-flipRight -sc40">
+                            <img src="https://via.placeholder.com/475x320/?text=480x320">
+                            <p class="-right a-fade -sp4"><span class="s-kome -color03">※イメージ</span></p>
+                        </li>
+                    </ul>
+                </article>
+
             </div>
+
+            <!-- タブ + 画像モーダル -->
+            <TabComponent/>
         </div>
-        <!-- ---------------- 【SP2カラム、PC2カラム】 ------------------- -->
-        <div class="c-contents w95 mgb10 mgb10s">
-            <article>
-                <ul class="c-flex -h-c_pc -col2_2 -r mgb10 mgb10s">
-                    <li class="">
-                        <p class="c-contents_subTitle s-L -left mgb5 mgb5s a-fadeRight -sc40">コンテンツアイテム_タイトル</p>
-                        <p class="s-M -lh-2 -lh-1_5_s -left a-fadeRight -sc40">
-                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
-                        </p>
-                    </li>
-                    <li class=" a-flipLeft -sc40">
-                        <img src="https://via.placeholder.com/475x320/?text=480x320">
-                        <p class="-right a-fade -sp4"><span class="s-kome -color03">※イメージ</span></p>
-                    </li>
-                </ul>
-            </article>
-
-            <article>
-                <ul class="c-flex -h-c_pc -col2_2">
-                    <li class="">
-                        <p class="c-contents_subTitle s-L -left mgb5 mgb5s a-fadeLeft -sc40">コンテンツアイテム_タイトル</p>
-                        <p class="s-M -lh-2 -lh-1_5_s -left a-fadeLeft -sc40">
-                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
-                        </p>
-                    </li>
-                    <li class="a-flipRight -sc40">
-                        <img src="https://via.placeholder.com/475x320/?text=480x320">
-                        <p class="-right a-fade -sp4"><span class="s-kome -color03">※イメージ</span></p>
-                    </li>
-                </ul>
-            </article>
-
-        </div>
-
-        <!-- タブ + 画像モーダル -->
-        <TabComponent/>
     </div>
-  </div>
 </template>
